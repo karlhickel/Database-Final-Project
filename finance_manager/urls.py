b6194a472a2450ce375f853256f0b7ba97e115c8
ander428 <ander428@mail.chapman.edu>
2019-04-26 00:19:42 -0700

    Authentication Working

--- finance_manager/urls.py
+++ finance_manager/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name="financeManager"
 urlpatterns = [
     # dictate the possible url paths of the app
     path('', views.home, name='financials-home'),
-    path('dev/', views.dev, name='development-page'),
     path('login/', views.login, name='auth-page'),
+    path('signup/', views.signup, name='signup-page'),
     path('analytics/', views.analytics, name='analytics-page'),
     path('transactions/', views.trans, name='trans-page')
 ]
--- finance_manager/views.py
+++ finance_manager/views.py
@@ -3,8 +3,7 @@ from django.conf import settings
 from django.shortcuts import render
 from .static.financeManager.python.SQLManager import SQLManager
 from .static.financeManager.python.utils import utils
-from .static.financeManager.python.forms import LoginForm
-
+from .static.financeManager.python.forms import LoginForm, SignupForm
 
 
 # create MySQL connection
@@ -21,26 +20,87 @@ sqlPath = os.path.join(os.getcwd(), "finance_manager", "static", "financeManager
 
 ### Functions for rendering each page ###
 
+data = {'ander428': 'Password1', 'jwanderson198': 'Password1'}
+args = {
+    'title': "",
+    'form': None,
+    'err': {},
+    'UserName': "",
+}
+
 # login page
 def login(request):
-    form = LoginForm()
-    return render(request, "financeManager/login.html", {'title': 'Log In', 'form': form})
+    global args
+    args['title'] = "Login"
+
+    err = {'UserName': False, 'Password': False}
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            usr = form.cleaned_data['userName']
+            psw = form.cleaned_data['password']
+
+            if usr in data:
+                if data[usr] == psw:
+                    print("Login successful!")
+                    args['UserName'] = usr
+                    return home(request)
+                else:
+                    err['Password'] = True
+                    print("Att:", psw, "Act:", data[usr])
+            else:
+                err['UserName'] = True
+                print("Att:", usr)
+
+
+    args['form'] = LoginForm()
+    args['err'] = err
+    return render(request, "financeManager/login.html", args)
+
+# signup page
+def signup(request):
+    global args
+    print(data)
+    err = {'UserName': False, 'Password': False}
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            usr = form.cleaned_data['userName']
+            psw = form.cleaned_data['password']
+            cfm = form.cleaned_data['confirmPassword']
+
+            if usr not in data:
+                if psw == cfm:
+                    data.setdefault(usr, psw)
+                    print("Account Created")
+                    print(data)
+                else:
+                    err['Password'] = True
+            else:
+                err['UserName'] = True
+                print("Att:", usr)
+
+    args['form'] = SignupForm()
+    args['err'] = err
+    return render(request, "financeManager/signup.html", args)
 
 # home page
 def home(request):
-    return render(request, "financeManager/home.html", {'title': 'Home'})
+    global args
+    args['title'] = "Home"
+    print(args['UserName'])
+    return render(request, "financeManager/home.html", args)
 
 # analytics page
 def analytics(request):
-    return render(request, "financeManager/analytics.html", {'title': 'Analytics'})
+    global args
+    args['title'] = "Analytics"
+    return render(request, "financeManager/analytics.html", args)
 
 # transactions page
 def trans(request):
-    return render(request, "financeManager/transactions.html", {'title': 'Transactions'})
-
-# developement page
-def dev(request):
-    context = {
-        'records': records
-    }
-    return render(request, "financeManager/dev.html", context)
+    global args
+    args['title'] = "Transactions"
+    return render(request, "financeManager/transactions.html", args)
